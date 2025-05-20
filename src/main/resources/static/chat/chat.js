02d67a0d827e6a7711fbf4ffbad4b2f6d1d06881
Antoni Vasilev <antonivasilev15@gmail.com>
2025-05-21 01:55:03 +0300

    - README.MD

---
+++ src/main/resources/static/chat/chat.js
@@ -0,0 +1,81 @@
+// Провери дали има логнат потребител
+const currentUser = localStorage.getItem('currentUser');
+if (!currentUser) {
+    window.location.href = 'login';
+}
+
+const messagesDiv = document.getElementById('messages');
+const messageForm = document.getElementById('message-form');
+const messageInput = document.getElementById('message-input');
+const username = currentUser.username || 'Анонимен';
+
+// Свържи се към socket.io сървъра
+const socket = io();
+
+// При изпращане на съобщение
+messageForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const text = messageInput.value.trim();
+    if (!text) return;
+    // socket.emit('chat message', { username, text });
+
+    fetch('/message', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', 'Authorization': currentUser},
+        body: JSON.stringify({
+            "message": text,
+        })
+    }).then(r => r.json())
+        .then(data => {
+            console.log(data);
+        })
+
+    messageInput.value = '';
+});
+
+// При получаване на съобщение
+// socket.on('chat message', function (msg) {
+//     addMessage(msg);
+// });
+
+let lastMessageDate = null;
+setInterval(() => {
+    fetch('/message/get', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', 'Authorization': currentUser},
+        body: JSON.stringify({
+            "getOldMessages": lastMessageDate == null ? true : false,
+            "currentDate": lastMessageDate
+        })
+    }).then(r => r.json())
+        .then(data => {
+            data.forEach(msg => {
+                addMessage(msg);
+            });
+            lastMessageDate = data[data.length - 1].sentDate
+        })
+}, 1000);
+
+// Добавяне на съобщение в DOM
+function addMessage(msg) {
+    const msgDiv = document.createElement('div');
+    msgDiv.classList.add('message');
+    if (msg.itsMe) msgDiv.classList.add('self');
+
+    const userSpan = document.createElement('span');
+    userSpan.className = 'username';
+    userSpan.textContent = msg.senderUsername;
+
+    const textSpan = document.createElement('span');
+    textSpan.className = 'text';
+    textSpan.textContent = msg.message;
+
+    msgDiv.appendChild(userSpan);
+    msgDiv.appendChild(textSpan);
+    messagesDiv.appendChild(msgDiv);
+
+    // Скролирай надолу
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+}
+
+// (По желание) Може да добавиш съобщение "Добре дошъл" при влизане
