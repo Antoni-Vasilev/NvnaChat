bc4ad9c1a0d2bac5195271831fbb609beaad2cf6
Antoni Vasilev <antonivasilev15@gmail.com>
2025-05-21 02:02:07 +0300

    Fix dotenv load

--- src/main/java/bg/nvna/nvnachat/NvnaChatApplication.java
+++ src/main/java/bg/nvna/nvnachat/NvnaChatApplication.java
@@ -1,5 +1,6 @@
 package bg.nvna.nvnachat;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NvnaChatApplication {
 
     public static void main(String[] args) {
+        Dotenv.load().entries().forEach(it -> System.setProperty(it.getKey(), it.getValue()));
         SpringApplication.run(NvnaChatApplication.class, args);
     }
 
